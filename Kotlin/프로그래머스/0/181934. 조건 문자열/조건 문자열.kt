ddfb9a3d2a0edce66efb4d971d33ebd9870ff264
isanghoony <isanghoony@gmail.com>
2025-07-25 01:26:37 +0900

    [level 0] Title: 조건 문자열, Time: 2.13 ms, Memory: 62.8 MB -BaekjoonHub

--- Kotlin/프로그래머스/0/181934. 조건 문자열/조건 문자열.kt
+++ Kotlin/프로그래머스/0/181934. 조건 문자열/조건 문자열.kt
@@ -1,14 +1,13 @@
 class Solution {
     fun solution(ineq: String, eq: String, n: Int, m: Int): Int {
         var answer: Int = 0
-        
-        answer = 
-        if(ineq == ">" && eq == "=" && n >= m) 1
-        else if(ineq == "<" && eq == "=" && n <= m) 1
-        else if(ineq == ">" && eq == "!" && n > m) 1
-        else if(ineq == "<" && eq == "!" && n < m) 1
-        else 0
-        
+        answer = when (ineq + eq) {
+            ">=" -> if(n >= m) 1 else 0
+            "<=" -> if(n <= m) 1 else 0
+            ">!" -> if(n > m) 1 else 0
+            "<!" -> if(n < m) 1 else 0
+            else -> 0
+        }
         return answer
     }
 }
