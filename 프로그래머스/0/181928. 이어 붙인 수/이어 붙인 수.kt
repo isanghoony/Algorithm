adbf1360d24e662556a88d2a61796d442d46d4e1
isanghoony <isanghoony@gmail.com>
2025-08-04 13:25:54 +0900

    Delete 프로그래머스/0 directory

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.kt
+++
@@ -1,9 +0,0 @@
-class Solution {
-    fun solution(num_list: IntArray): Int {
-        var answer: Int = 0
-        val oddStr = num_list.filter { it % 2 == 1 }.joinToString("")
-        val evenStr = num_list.filter { it % 2 == 0 }.joinToString("")
-        
-        return if(oddStr.isEmpty()) 0 else oddStr.toInt() + if(evenStr.isEmpty()) 0 else evenStr.toInt()
-    }
-}
